4e28241433e8ca9dc571f0cabd67332479d2fd7c
Deman250 <deman.250052@gmail.com>
2019-11-26 18:20:49 +0200

    add mask

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,29 +1,34 @@
 'use strict';
 
-$('#form').validate({
-  rules: {
-    name: {
-      required: true,
+$(document).ready(function() {
+  $('#form').validate({
+    rules: {
+      name: {
+        required: true,
+        // asd
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+      phone: {
+        required: true,
+        number: true,
+        minlength: 10,
+        maxlength: 10,
+      },
+      country: 'required',
+      bday: 'date',
+      password: {
+        required: true,
+        minlength: 8,
+      },
+      password2: {
+        required: true,
+        minlength: 8,
+        equalTo: '#password',
+      },
     },
-    email: {
-      required: true,
-      email: true,
-    },
-    phone: {
-      required: true,
-      minlength: 10,
-      number: true,
-    },
-    country: {
-      required: true,
-    },
-    password: {
-      required: true,
-      minlength: 8,
-    },
-    password2: {
-      required: true,
-      minlength: 8,
-    },
-  },
+    messages: { password2: { equalTo: 'Passwords do not match' } },
+  });
 });
